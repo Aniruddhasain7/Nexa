3d6c37243c208f2a3f766a78433d5d0ba4a65525
Aniruddha Sain <aniruddhasain315@gmail.com>
2026-04-12 09:38:29 +0530

    update loading

--- client/src/pages/Loading.jsx
+++ client/src/pages/Loading.jsx
@@ -14,7 +14,7 @@ const Loading = () => {
     return ()=> clearTimeout(timeout)
   },[])
   return (
-    <div className='bg-gradient-to-b from-[#000000] to-[#181b4b]
+    <div className='bg-gradient-to-b from-[#242124] to-[#000000]
     backdrop-opacity-60 flex items-center justify-center h-screen w-screen
     text-white text-2xl'>
       <div className='w-10 h-10 rounded-full border-3 border-white
